e8697b1ecfe0eadfbff8578975e33cbfb4bde097
jafarjtown <57459852+jafarjtown@users.noreply.github.com>
2021-12-12 01:40:33 +0100

    Add files via upload

---
+++ friends/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+app_name = 'friends'
+# from rest_framework import routers
+# from .serializers import F
+# router = routers.DefaultRouter()
+
+# router.register('activities', )
+
+urlpatterns = [
+    path('', views.Followers, name='friends'),
+    path('request', views.RequestFriends, name='request_friends'),
+    path('follow/<slug:pk>', views.Follow, name='follow'),
+    path('unfollow/<slug:pk>', views.unFollow, name='unfollow'),
+]
---
+++ friends/views.py
@@ -0,0 +1,43 @@
+from django.http.response import JsonResponse
+from django.shortcuts import render
+from accounts.models import User
+from django.db.models.query import Q
+from notifications.notifcations import FriendRequestNotification
+# Create your views here.
+
+
+def Followers(request):
+    user = request.user
+    friends = User.objects.filter(Q(faculty=user.faculty) | Q(department = user.department)).exclude(username = user.username)
+    f = []
+    for friend in friends.all():
+        if friend not in user.followings.all():
+            f.append(friend)
+    context = {
+        'suggestions': f
+    }
+    return render(request, 'friends/index.html', context)
+
+def RequestFriends(request):
+    return render(request, 'friends/s_request.html')
+
+def Follow(request, pk):
+    to = User.objects.get(id=pk)
+    sender = request.user
+    sender.followings.add(to)
+    to.followers.add(sender)
+    FriendRequestNotification(to, sender, 'follow').send()
+    return JsonResponse({
+        'response': 'successfull',
+        'friend': f'{to.first_name} {to.last_name}'
+    })
+
+def unFollow(request, pk):
+    to = User.objects.get(id=pk)
+    sender = request.user
+    sender.followings.remove(to)
+    to.followers.remove(sender)
+    return JsonResponse({
+        'response': 'successfull',
+        'friend': to.username
+    })

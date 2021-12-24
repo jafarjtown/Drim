41c6733c2f70e9789bb815bc16edc0faa87a5bf2
jafarjtown <57459852+jafarjtown@users.noreply.github.com>
2021-12-24 01:01:52 +0100

    Add files via upload

--- helpers/models.py
+++ helpers/models.py
@@ -4,8 +4,8 @@ import uuid
 
 class ModelHelper(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    # created_at = models.DateTimeField(auto_now_add=True)
-    # updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_created=True)
 
     class Meta:
         abstract = True

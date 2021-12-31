e88e0a34a5c51b854a56e55a466182683e79e501
jafarjtown <57459852+jafarjtown@users.noreply.github.com>
2021-12-31 23:33:12 +0100

    Add files via upload

--- institution/models.py
+++ institution/models.py
@@ -4,31 +4,41 @@ from helpers.models import ModelHelper
 
 
 class Institution(ModelHelper):
+    administrator = models.OneToOneField('accounts.User', on_delete=models.SET_NULL, null = True, related_name='administrator')
+    tutors = models.ManyToManyField('accounts.Teacher', blank=True)
     name = models.CharField(max_length=500, unique=True)
+    slogan = models.CharField(max_length=500)
+    logo = models.FileField()
+    cover_img = models.FileField()
+    website_url = models.URLField()
+    address = models.TextField()
+    verified = models.BooleanField(default=False)
+    blogs = models.ManyToManyField('posts.Post', related_name='institution')
+    activities = models.ManyToManyField('activities.Activity')
     materials = models.ManyToManyField('Files.File', blank=True)
     events = models.ManyToManyField('Event', blank=True)
     programmes = models.ManyToManyField('Programme', blank=True, related_name='institution')
+    courses = models.ManyToManyField('Course', blank=True)
     pages = models.ManyToManyField("pages.Page", blank=True)
-
+    groups = models.ManyToManyField('groups.Group', related_name='institution')
+    subscribers = models.ManyToManyField('accounts.Student', related_name='institutions_subscribes')
+    
     @staticmethod 
     def HigherSubscribe():
-        inst = Institution.objects.all()
+        inst = Institution.objects.only('subscribers')
         higher = 0
         winner = []
         for h in inst.all():
             if h.subscribers.count() > higher:
+                winner = []
                 higher = h.subscribers.count()
                 winner.append({ 'name': h.name , 'number_of_sub' : higher})
             elif h.subscribers.count() == higher:
-                prev = winner
-                print(prev)
                 winner.append({'name': h.name, 'number_of_sub' : higher})
         if len(winner) == 1:
             return winner[0]
         else:
             return winner
-
-
 class Event(ModelHelper):
     name = models.CharField(max_length=100)
     venue = models.CharField(max_length=500)
@@ -39,3 +49,7 @@ class Programme(ModelHelper):
     name = models.CharField(max_length=100)
     materials = models.ManyToManyField('Files.File', blank=True, related_name='programme')
     description = models.TextField()
+
+class Course(ModelHelper):
+    title = models.CharField(max_length=100)
+    slogan = models.CharField(max_length=200)
--- institution/serializers.py
+++ institution/serializers.py
@@ -1,12 +1,43 @@
 from django.db.models import fields
 from rest_framework.serializers import ModelSerializer
 from rest_framework.viewsets import ModelViewSet
+from accounts.serializers import UserSerialzer
+
+from posts.models import Post
 from .models import Institution
 class InstitutionSerializer(ModelSerializer):
     class Meta:
         model = Institution
-        fields = '__all__'
+        exclude= ['blogs']
 
 class InstitutionViewSet(ModelViewSet):
     serializer_class = InstitutionSerializer
     queryset = Institution.objects.all()
+
+
+class instSetSerializerPost(ModelSerializer):
+    class Meta:
+        model = Institution
+        fields = ['name','logo', 'id','verified']
+
+class PostSerializer(ModelSerializer):
+    author = UserSerialzer()
+    institution = instSetSerializerPost(many=True)
+    class Meta:
+        model = Post
+        fields = '__all__'
+        depth = 3
+
+
+class InstitutionBlogsSerializer(ModelSerializer):
+    blogs = PostSerializer(many=True)
+    class Meta:
+        model = Institution
+        fields = ['blogs']
+        
+        depth = 3
+
+class InstitutionBlogsViewSet(ModelViewSet):
+    serializer_class = InstitutionBlogsSerializer
+    queryset = Institution.objects.all()
+    
